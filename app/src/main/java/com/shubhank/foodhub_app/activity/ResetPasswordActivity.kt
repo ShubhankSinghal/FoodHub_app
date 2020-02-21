5612fcee4f42be17d40f2d6cb7ee48af2b0cb73f
ShubhankSinghal <shubhank.singhal98@gmail.com>
2020-02-21 22:31:41 +0530

    UI Preparation

---
+++ app/src/main/java/com/shubhank/foodhub_app/activity/ResetPasswordActivity.kt
@@ -0,0 +1,13 @@
+package com.shubhank.foodhub_app.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shubhank.foodhub_app.R
+
+class ResetPasswordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otp)
+    }
+}
